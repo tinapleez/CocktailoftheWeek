c6f5c2bf0759279ddd5bb7a4b4b56771e64e7e90
tinapleez <tinapleeez@gmail.com>
2018-07-18 00:47:02 -0400

    Add Preference orderBy and bind to value

--- app/src/main/java/com/freecbdhomebiz/cocktailoftheweek/SettingsActivity.java
+++ app/src/main/java/com/freecbdhomebiz/cocktailoftheweek/SettingsActivity.java
@@ -8,6 +8,7 @@ package com.freecbdhomebiz.cocktailoftheweek;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -29,11 +30,27 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderBy);
         }
 
         @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-            return false;
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            // The code in this method takes care of updating the displayed preference summary
+            // after it has been changed
+            String stringValue = value.toString();
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+            } else {
+                preference.setSummary(stringValue);
+            }
+            return true;
         }
 
         private void bindPreferenceSummaryToValue(Preference preference) {
